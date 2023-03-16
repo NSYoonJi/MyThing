d3c6e860c22647cf6fe9f85d2ea1256c285bc251
yooooonzzzzzang <cherishvalue@naver.com>
2023-03-16 14:07:02 +0900

    FEAT: BE_application.yml 생성

---
+++ back/src/main/java/com/project/mything/MythingApplication.java
@@ -0,0 +1,13 @@
+package com.project.mything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MythingApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/project/mything/MythingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MythingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
