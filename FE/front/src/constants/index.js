e991a0fae8404c5e8e6c79ccdb38c5046fd34e92
ehdrmfdl002 <67990029+ehdrmfdl002@users.noreply.github.com>
2023-04-13 14:03:47 +0900

    Update index.js

--- FE/front/src/constants/index.js
+++ FE/front/src/constants/index.js
@@ -1,5 +1,5 @@
-export const API_END_POINT = "https://makeyourpreference.com/api";
-export const END_POINT = "https://makeyourpreference.com";
+export const API_END_POINT = "http://makeyourpreference.com/api";
+export const END_POINT = "http://makeyourpreference.com";
 
 // local, server
 export const mode = "server";
