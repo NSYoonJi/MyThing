8a543b1be2db4520c57bd987546709a34f379795
jhjo01 <109667442+jhjo01@users.noreply.github.com>
2023-04-13 14:47:07 +0900

    Update index.js

--- FE/front/src/constants/index.js
+++ FE/front/src/constants/index.js
@@ -1,5 +1,5 @@
 export const API_END_POINT = "http://makeyourpreference.com/api";
-export const END_POINT = "http://makeyourpreference.com";
+export const END_POINT = "https://makeyourpreference.com";
 
 // local, server
 export const mode = "server";
